0261b459a5970be0a6c5254dbae439d3f1ced146
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-10-04 18:14:39 -0400

    Fixed the errors inside the Adapter class, and implemented WordViewHolder

--- RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.recyclerview;
 
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -26,6 +27,13 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     class WordViewHolder extends RecyclerView.ViewHolder{
         public final TextView wordItemView;
         final WordListAdapter mAdapter;
+
+        public WordViewHolder(View itemView, WordListAdapter adapter){
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+
+        }
 
     }
 }
