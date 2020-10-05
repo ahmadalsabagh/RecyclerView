d0a31c25b29a680298419035db74109e0da64be8
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-10-04 20:42:36 -0400

    Removed the placeholder text and added the real text

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -18,14 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Put initial data into the word list.
-        for (int i = 0; i < 20; i++) {
-            mWordList.addLast("Word " + i);
-        }
 
-        for (int i = 0; i < 20; i++) {
-            mDescriptionList.addLast("Wordzzzz " + i);
-        }
+        mWordList.add("Shawrma Hummus");
+        mDescriptionList.add("You will only try this in the arabian lands. Trust me it is one of the best things you will ever taste" +
+                " in your entire dull life");
+
+        mWordList.add("Tabouleh Salad");
+        mDescriptionList.add("Dont even get me started. If you've never tried this you are living under a rock");
+
+        mWordList.add("Manakeesh");
+        mDescriptionList.add("You will cry tears of happiness while eating a bite of this");
+
+        mWordList.add("Falafel");
+        mDescriptionList.add("If you're broke and cannot afford much, try this out. It's the arab equivalent of instant noodles.");
+
+        mWordList.add("Wara' Anab");
+        mDescriptionList.add("It might not look appetising but once you eat one there's no stopping.");
+
+        mWordList.add("Bizza");
+        mDescriptionList.add("Common misconception to think this is pizza, its not. It's added arab deliciousness.");
+
+        mWordList.add("Chicken Nuggets");
+        mDescriptionList.add("You can never go wrong with chicken nuggets!");
+
+        mWordList.add("Yummy Nuggets");
+        mDescriptionList.add("An alternative to chicken nuggets");
+
+        mWordList.add("Burger King");
+        mDescriptionList.add("Have it your way");
+
+        mWordList.add("Mcdonalds");
+        mDescriptionList.add("Disgusting. But some people like it.");
 
         // Get a handle to the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerview);
