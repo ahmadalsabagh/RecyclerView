f33f68556816d551028ff37f16a96d7e5984dba4
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-10-04 22:32:12 -0400

    UP Button works

--- RecyclerView/app/src/main/java/com/example/recyclerview/RecipeActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/RecipeActivity.java
@@ -5,8 +5,11 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
         Log.d("TAG", "onClick: clicked on: 2 ");
+
         getIncomingIntent();
     }
 
