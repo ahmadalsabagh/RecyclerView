76bf92bdd784b79989618e1d86ec10f9b0f604c8
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-10-09 21:29:08 -0400

    Successfully completed adding the splash music for 5 seconds

--- RecyclerView/app/src/main/java/com/example/recyclerview/Splash.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Splash.java
@@ -3,26 +3,34 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
+    MediaPlayer splash_music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Initialize the music
+        splash_music = new MediaPlayer();
+        splash_music = MediaPlayer.create(this,R.raw.sound);
         setContentView(R.layout.activity_splash);
-        TimerTask task=new TimerTask() {
+        TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 finish();
                 startActivity(new Intent(Splash.this, FoodRecipeListActivity.class));
+                splash_music.pause();
 
             }
         };
-        Timer oppening= new Timer();
-        oppening.schedule(task,5000);
+        Timer opening = new Timer();
+        splash_music.start();
+        opening.schedule(task,5000);
+
     }
 }
