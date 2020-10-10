440923925b57ee6ff9153355867b7efa651787cb
ahmadalsabagh <ahmad-alsabagh@hotmail.com>
2020-10-09 20:54:57 -0400

    Added the splash page successfully

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Splash.java
@@ -0,0 +1,28 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(Splash.this, FoodRecipeListActivity.class));
+
+            }
+        };
+        Timer oppening= new Timer();
+        oppening.schedule(task,5000);
+    }
+}
